8124829a587939119cc6bdf825905ece6a9d4984
KangJiHyung <wlgud614@google.com>
2015-12-12 18:34:44 +0900

    Implicit Intent (암시적 인텐트)

---
+++ app/src/main/java/com/example/cherry/implicit_intent/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cherry.implicit_intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button)findViewById(R.id.button);
+
+
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("http://www.naver.com/");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+    }
+}
